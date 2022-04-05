12dc3950c85228167c71ecedf68141b2331c22ca
GilbertoASJ <alvesgilberto84@gmail.com>
2022-04-05 15:43:16 -0300

    Feature: :sparkles: Data biding and life cicle hooks

--- vuejs-introduction/src/components/Form.vue
+++ vuejs-introduction/src/components/Form.vue
@@ -1,11 +1,14 @@
 <template>
-	
-	<form id="app-form">
+
+	<form id="app-form">	
 		<input type="text" v-model="input_name" placeholder="Digite seu nome">
 		<input type="submit" name="submit" value="Enviar" @click="handleSubmitForm">
 
-		<p>Olá {{ name }}</p>
 		<p>Olá {{ input_name }}</p>
+		<p>
+			Olá <strong>{{ name }}</strong>, 
+			fiquei sabendo que você é <strong>{{ profession }}</strong>
+		</p>
 	</form>
 </template>
 
@@ -17,12 +20,14 @@
 			return {
 				name: "",
 				input_name: "", 
+				profession: "Programador",
 			}
 		},
 		methods: {
 
 			handleSubmitForm(e) {
 
+				// Recupera o valor de name e atribui a input_name, assim quando input name for exibido em tela, seu valor irá ser calculado apenas após o clique do botão
 				this.name = this.input_name;
 				e.preventDefault();
 			}
---
+++ vuejs-introduction/src/components/LifeCycle.vue
@@ -0,0 +1,29 @@
+<template>
+	<h3>Meu nome é {{ name }}</h3>
+</template>
+
+<script>
+	
+	export default {
+		name: 'LifeCycle',
+		data() {
+			return {
+				name: 'Não definido'
+			}
+		},
+		created() {
+
+			setTimeout(() => {
+
+				this.name = 'Gilberto';
+			}, 1000)
+		},
+		mounted() {
+
+			setTimeout(() => {
+
+				this.name = 'Júnior';
+			}, 2000)
+		}
+	}
+</script>
