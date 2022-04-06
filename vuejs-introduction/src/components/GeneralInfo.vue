2b4ba31006b906dde5849538ca1bbecb404f70b1
GilbertoASJ <alvesgilberto84@gmail.com>
2022-04-06 16:28:23 -0300

    Feature: :sparkles: Directives - v-if v-else v-show

--- vuejs-introduction/src/components/GeneralInfo.vue
+++ vuejs-introduction/src/components/GeneralInfo.vue
@@ -1,11 +1,17 @@
 <template>
 	<div>
-		<p>No momento está trabalhando com as seguintes tecnologias:</p>
+		<!-- Diretivas if else -->
+		<p v-if="isWorking">Estou trabalhando :)</p>
+		<p v-else>No momento estou em busca de novas oportunidades</p>
+		<p>Estou utilizando no momento as seguintes tecnologias: </p>
 		<ul>
 			<li>HTML e CSS</li>
 			<li>Javascript com Vuejs</li>
 			<li>Back-end com PHP</li>
 		</ul>
+
+		<!-- Diretiva simplista para exibir informação na tela -->
+		<p v-show="showEmail">Mande uma mensagem para: {{ email }}</p>
 	</div>
 </template>
 
@@ -13,5 +19,12 @@
 	
 	export default {
 		name: 'GeneralInfo',
+		data() {
+			return {
+				isWorking: true,
+				showEmail: true,
+				email: 'gilberto@gmail.com'
+			}
+		}
 	}
 </script>
