2e108166afea5ec035e300282d5300eee2e710b6
GilbertoASJ <alvesgilberto84@gmail.com>
2022-04-13 16:18:39 -0300

    Feature: :sparkles: $emit

---
+++ vuejs-introduction/src/components/ChangeImage.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<button @click="$emit('changeImage')">Trocar imagem de perfil</button>
+	</div>
+</template>
+
+<script>
+	
+	export default {
+		name: "ChangeImage",
+		emits: ['changeImage']
+	}
+</script>
+
+<style scoped>
+	
+	button {
+		background: transparent;
+		border: 1px solid whitesmoke;
+		padding: 5px;
+		color: whitesmoke;
+		cursor: pointer;
+		transition: .95s;
+	}
+
+	button:hover {
+		background: whitesmoke;
+		color: black;
+	}
+</style>
