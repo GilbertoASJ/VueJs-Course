493bdf1c36e7c82b416c8d667bc479d102db6347
GilbertoASJ <alvesgilberto84@gmail.com>
2022-04-13 16:18:09 -0300

    Feature: :sparkles: $emit

--- vuejs-introduction/src/components/UserPicture.vue
+++ vuejs-introduction/src/components/UserPicture.vue
@@ -1,15 +1,38 @@
 <template>
 	<!-- Ao invés de utilizar a forma escrita 'v-bind', podemos utilizar um 'short-code', assim utilizando apenas ':' -->
 	<img :src="avatarImg" :alt="avatarImgAlt">
+	<ChangeImage @change-image="changeUserPicture" />
 </template>
 
 <script>
+
+	import ChangeImage from './ChangeImage.vue'
+
 	export default {
 		name: 'UserPicture',
+		components: {
+			ChangeImage,
+		},
 		data() {
 			return {
 				avatarImg: 'https://avatars.githubusercontent.com/u/56325350?v=4',
 				avatarImgAlt: 'Imagem do avatar do usuário'
+			}
+		},
+		methods: {
+			changeUserPicture() {
+
+				let firstImage = "https://avatars.githubusercontent.com/u/56325350?v=4"
+				let secondImage = "https://github.com/GilbertoASJ/GilbertoASJ/raw/master/Code%20typing-bro.png"
+
+				if(this.avatarImg == firstImage) {
+
+					this.avatarImg = secondImage
+
+				} else if(this.avatarImg == secondImage) {
+					this.avatarImg = firstImage;
+				}
+
 			}
 		}
 	}
